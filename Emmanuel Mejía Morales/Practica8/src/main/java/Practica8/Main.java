1fabe2bef17186ff3ab385ebf04fff34a4c28a64
EmmanuelMMAstratos <78919881+EmmanuelMMAstratos@users.noreply.github.com>
2021-03-03 10:08:28 -0600

    Proyecto8
    aqui mi proyecto 8 :3

---
+++ Emmanuel Mejía Morales/Practica8/src/main/java/Practica8/Main.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practica8;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author armma
+ */
+public class Main 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+        try
+        {/*
+        ArrayList<Usuario> arreglo=new ArrayList<>();
+        arreglo.get(0).getCorreo();
+            
+*/
+            int a=10,b=11;
+            if(a > b){
+        System.out.println("Todo esta bien");
+        //este en caso de que este bien
+        }
+        else{
+            //throw new ArrayIndexOutOfBoundsException();
+            throw new IOException();
+            }
+}
+        
+        //este es de jeraraquia menor
+        catch(IOException e)
+        {
+            System.out.println("Esta mal el arreglo por posicion de arreglo");   
+        }
+        //este es de jeraraquia mayor
+        catch(Exception e)
+        {
+            System.out.println("Este es el error"+e.getMessage());  
+        }
+        finally{
+            System.out.println("Aqui se puede agregar el dato");  
+        }
+       
+    } 
+}
