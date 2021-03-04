a28b7a63f6852734a4a2f711a88ad82ab308f65f
MarcoAntonioCondeLira <antoniocondelira@hotmail.com>
2021-03-03 21:12:38 -0600

    Manejo de errores.

---
+++ Marco Antonio Conde Lira/Practica9_Manejo_Errores/src/practica9_manejo_errores/Usuario.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica9_manejo_errores;
+
+/**
+ *
+ * @author Antonio Conde
+ */
+public class Usuario {
+    
+    //Variables de clase.
+    private String nombre;
+    private String correo;
+    
+    //Constructor
+    //public Usuario() {
+       // this.nombre = "";
+        //this.correo = "";
+   //}
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    
+}//End of class usuario
