0574fa85de2ff6d1ca90be4d37b2423e4bf15cf8
AxelHernandezCortes <78919736+AxelHernandezCortes@users.noreply.github.com>
2021-03-09 19:24:06 -0600

    Proyecto9
    aqui esta el proyecto 9

--- AxelHernandezCortes/Practica8/src/practica8/Practica8.java
+++ AxelHernandezCortes/Practica8/src/practica8/Practica8.java
@@ -1,31 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package practica8;
+package Practica8;
+
+import java.io.IOException;
 import java.util.ArrayList;
+
 /**
  *
  * @author Valeria
  */
-public class Practica8 {
+public class Practica8 
+{
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        try{
-            ArrayList<Usuario>arreglo= new ArrayList<>();
-            arreglo.get(0).getCorreo();
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+        try
+        {/*
+        ArrayList<Usuario> arreglo=new ArrayList<>();
+        arreglo.get(0).getCorreo();
+            
+*/
+            int a=10,b=11;
+            if(a > b){
+        System.out.println("Todo esta bien");
+        //este en caso de que este bien
         }
-        catch(ArrayIndexOutOfBoundsException e){
-            System.out.println("Acabas de entrar en un error de posicion de arreglo"+e.getMessage());
-        }
-        catch(Exception e){
-            System.out.println("Aca pueden poner codigo ue quieren t+yty+  ");
-        }
-        
-    }
-    
+        else{
+            //throw new ArrayIndexOutOfBoundsException();
+            throw new IOException();
+            }
 }
+        
+        //este es de jeraraquia menor
+        catch(IOException e)
+        {
+            System.out.println("Esta mal el arreglo por posicion de arreglo");   
+        }
+        //este es de jeraraquia mayor
+        catch(Exception e)
+        {
+            System.out.println("Este es el error"+e.getMessage());  
+        }
+        finally{
+            System.out.println("Aqui se puede agregar el dato");  
+        }
+       
+    }}
