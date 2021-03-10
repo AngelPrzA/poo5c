6f4d64393734ff30bdb0891d98d00e1087d401c1
Arendy Cuahutencos Diyarza <79030147+Arendy5@users.noreply.github.com>
2021-03-10 14:48:05 -0600

    Practica10

---
+++ Arendy Cuahutencos Diyarza/Practica10/src/main/java/Practica10/Persona.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practica10;
+
+/**
+ *
+ * @author arendycuahutencosdiyarza
+ */
+public class Persona {
+  /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+    private String id;
+    private String imagen;
+    private String edad;
+    private String nombre;
+    private String compaÒÌa;
+    private String email;
+    private String telefono;
+    private String direccion;
+    private String  acerca_de;
+    
+    public Persona(){
+        this.id = "";
+        this.imagen = "";
+        this.edad = "";
+        this.nombre = "";
+        this.compaÒÌa = "";
+        this.email = "";
+        this.telefono = "";
+        this.direccion = "";
+        this.acerca_de = "";
+    }
+
+   
+    
+}
+  
+}
