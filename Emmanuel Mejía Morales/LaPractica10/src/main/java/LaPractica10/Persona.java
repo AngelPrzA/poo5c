038e217413fd0a68c9381e7d73924d56b2977521
EmmanuelMMAstratos <78919881+EmmanuelMMAstratos@users.noreply.github.com>
2021-03-10 10:31:43 -0600

    subida de practica 10
    practica 10

---
+++ Emmanuel Mejía Morales/LaPractica10/src/main/java/LaPractica10/Persona.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LaPractica10;
+
+/**
+ *
+ * @author armma
+ */
+public class Persona {
+    private String id;
+    private String imagen;
+    private String edad;
+    private String nombre;
+    private String compañía;
+    private String email;
+    private String telefono;
+    private String direccion;
+    private String  acerca_de;
+    
+    public Persona(){
+        this.id = "";
+        this.imagen = "";
+        this.edad = "";
+        this.nombre = "";
+        this.compañía = "";
+        this.email = "";
+        this.telefono = "";
+        this.direccion = "";
+        this.acerca_de = "";
+    }
+
+   
+    
+}
