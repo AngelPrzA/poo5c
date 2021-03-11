fd794850be2346dcf82123b560691fddeffe2d28
Arendy Cuahutencos Diyarza <79030147+Arendy5@users.noreply.github.com>
2021-03-10 21:23:48 -0600

    Practica10

--- Arendy Cuahutencos Diyarza/Practica10/src/main/java/UPTlax/Practica10/Main.java
+++ Arendy Cuahutencos Diyarza/Practica10/src/main/java/UPTlax/Practica10/Main.java
@@ -3,24 +3,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Practica10;
+package UPTlax.Practica10;
 
 /**
  *
  * @author arendycuahutencosdiyarza
  */
 public class Main {
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         Archivo archivo = new Archivo("archivos/personas.txt"); 
         String temp = archivo.leer();
         System.out.println(temp);
         
         archivo.escribir("archivos/nuevo_archivo.txt", false);
-    }
-    
-    
+    }   
 }
+
