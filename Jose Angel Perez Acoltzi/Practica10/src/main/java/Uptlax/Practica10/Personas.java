bbb6125ccc1e198d6fed379ae0e3201f43b04251
AngelPrzA <79215122+AngelPrzA@users.noreply.github.com>
2021-03-25 18:20:48 -0600

    Practica JSON

---
+++ Jose Angel Perez Acoltzi/Practica10/src/main/java/Uptlax/Practica10/Personas.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Uptlax.Practica10.clasesJSON;
+
+/**
+ *
+ * @author AngelPerez
+ */
+public class Personas {
+    private ArrayList<Persona> personas;
+    
+    public Personas(){
+        this.personas = new ArrayList<Persona>();
+    }
+
+    public ArrayList<Persona> getPersonas() {
+        return personas;
+    }
+}
+
+
