549e9056c35414c85125ca425de71d2568fce6ba
sabio <sabio@sabio-HP-ZBook-14>
2021-03-25 23:03:13 -0600

    Practica 12 de Serialización de objetos

---
+++ Ulises Serrano/Practica12/src/main/java/Practica12/Name.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practica12;
+
+/**
+ *
+ * @author sabio
+ */
+public class Name {
+    //Variables de clase
+    private String first;
+    private String last;
+    
+    //Constructor vacio
+    public Name(){
+        this.first = "";
+        this.last = "";
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+}
