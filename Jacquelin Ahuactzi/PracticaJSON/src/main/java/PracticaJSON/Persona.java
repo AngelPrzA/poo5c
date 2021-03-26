61b359e12cec1d892c8164214d990998934ec6b5
jacquelinahuactzi <71529228+jacquelinahuactzi@users.noreply.github.com>
2021-03-25 21:48:40 -0600

    Practica JSON

---
+++ Jacquelin Ahuactzi/PracticaJSON/src/main/java/PracticaJSON/Persona.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PracticaJSON;
+
+/**
+ *
+ * @author jacqu
+ */
+public class Persona {
+    //Variables de clase 
+    private String _Id;
+    private int index;
+    private String guid;
+    private boolean isActive;
+    private String balance;
+    private String picture;
+    private int age; 
+}
